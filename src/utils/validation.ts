83d3fd3946d5420c289cd314e32a4980f877a5a0
Mary <30400314+supercilium@users.noreply.github.com>
2023-02-28 00:18:24 +0300

    Feat/added prettier (#7)

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,58 +1,58 @@
 import { array, object, string, number } from "yup";
 
 export const createPartySchema = object({
-    // userName: yup.string().required(),
-    partyName: string().required(),
-    id: string().required(),
+  // userName: yup.string().required(),
+  partyName: string().required(),
+  id: string().required(),
 }).required();
 
 export const itemsSchema = object({
-    items: array().of(
-        object().shape({
-            name: string().required(),
-            price: number().min(0).default(0).required(),
-            amount: number().min(0).integer().required(),
-            discount: number().min(0).max(100).default(0),
-        })
-    ),
+  items: array().of(
+    object().shape({
+      name: string().required(),
+      price: number().min(0).default(0).required(),
+      amount: number().min(0).integer().required(),
+      discount: number().min(0).max(100).default(0),
+    })
+  ),
 }).required();
 
 export const addUserSchema = object({
-    identifier: string().required(),
+  identifier: string().required(),
 }).required();
 
 export const addItemSchema = object({
-    name: string().required(),
-    price: number().min(0).default(0).required(),
-    amount: number().min(1).integer().required(),
+  name: string().required(),
+  price: number().min(0).default(0).required(),
+  amount: number().min(1).integer().required(),
 }).required();
 
 export const signInSchema = object({
-    name: string().required(),
-    email: string().required(),
-    password: string().required(),
+  name: string().required(),
+  email: string().required(),
+  password: string().required(),
 }).required();
 
 export const loginSchema = object({
-    email: string().required(),
-    password: string().required(),
+  email: string().required(),
+  password: string().required(),
 }).required();
 
 export const partySettingsSchema = object({
-    discount: number()
-        .positive()
-        .test({
-            name: 'max',
-            exclusive: false,
-            params: {},
-            // eslint-disable-next-line no-template-curly-in-string
-            message: '${path} must be less than total price',
-            test: function (value) {
-                if (this.parent.isPercentage) {
-                    return (value || 0) <= 100
-                }
-                return (value || 0) <= parseFloat(this.parent.total)
-            },
-        })
-        .default(0),
+  discount: number()
+    .positive()
+    .test({
+      name: "max",
+      exclusive: false,
+      params: {},
+      // eslint-disable-next-line no-template-curly-in-string
+      message: "${path} must be less than total price",
+      test: function (value) {
+        if (this.parent.isPercentage) {
+          return (value || 0) <= 100;
+        }
+        return (value || 0) <= parseFloat(this.parent.total);
+      },
+    })
+    .default(0),
 }).required();
