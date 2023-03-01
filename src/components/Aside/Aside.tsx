cff7556b43a03000a07e5ec49bb8bfab4ebdae80
Mary <30400314+supercilium@users.noreply.github.com>
2023-03-01 19:29:10 +0300

    added useClickOutside (#8)

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,8 +1,15 @@
 import { FC, ReactNode } from "react";
 import { useUISettings } from "../../contexts/UIsettings";
+import { useClickOutside } from "../../hooks/useClickOutside";
 import { StyledAside } from "./Aside.style";
 
 export const Aside: FC<{ children: ReactNode }> = ({ children }) => {
-  const { isAsideVisible } = useUISettings();
-  return <StyledAside $isVisible={isAsideVisible}>{children}</StyledAside>;
+  const { isAsideVisible, setAsideVisibility } = useUISettings();
+  const ref = useClickOutside<HTMLDivElement>(() => setAsideVisibility(false));
+
+  return (
+    <StyledAside ref={ref} $isVisible={isAsideVisible}>
+      {children}
+    </StyledAside>
+  );
 };
