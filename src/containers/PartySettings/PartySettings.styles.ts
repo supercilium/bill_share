06cac914ddd002dfb06d09374a185d0dae529d04
Supercilium <marusel.12@gmail.com>
2023-03-14 14:22:25 +0300

    fixed bottom of party settings

--- src/containers/PartySettings/PartySettings.styles.ts
+++ src/containers/PartySettings/PartySettings.styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { device } from "../../components/styled/constants";
 
 export const SettingsRoot = styled.div`
-  padding-bottom: 5rem;
+  padding-bottom: 10rem;
   max-height: 100vh;
   overflow-y: auto;
+
+  @media ${device.tablet} {
+    padding-bottom: 5rem;
+  }
 `;
