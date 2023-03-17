43f08c875460b440293d82fa2b1244343d96e32e
Supercilium <marusel.12@gmail.com>
2023-03-17 20:40:58 +0300

    added sass & bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import "./bulma.min.css";
+import "./App.scss";
+// import "./bulma.min.css";
 import { Party } from "./pages/Party";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
