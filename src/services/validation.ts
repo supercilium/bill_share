1dda6c5212570e4223f09e9c57120da8a9dacb65
Supercilium <marusel.12@gmail.com>
2023-04-20 20:27:59 +0300

    added password confirmation validation

--- src/services/validation.ts
+++ src/services/validation.ts
@@ -1,5 +1,5 @@
 import { FieldValues, Path, UseFormSetError } from "react-hook-form";
-import { array, object, string, number } from "yup";
+import { array, object, string, number, ref } from "yup";
 import { ErrorRequest } from "../__api__/helpers";
 
 const name = string()
@@ -73,7 +73,10 @@ export const forgotPasswordSchema = object({
 
 export const resetPasswordSchema = object({
   password: password,
-  passwordConfirmation: password,
+  passwordConfirmation: string().oneOf(
+    [ref("password"), null],
+    "Passwords must match"
+  ),
 }).required();
 
 export const addItemSchema = object({
