f6a4340cb2f445cbb292b7f2f69d994ead496d91
Supercilium <marusel.12@gmail.com>
2023-07-01 13:36:16 +0300

    added saving party settings to LS

--- src/contexts/PartySettingsContext.tsx
+++ src/contexts/PartySettingsContext.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import React, { FC, useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { useParams } from "react-router";
 import { User } from "../types/user";
 import { partySettingsSchema } from "../services/validation";
 
@@ -15,25 +16,41 @@ export interface FormSettings {
   total: number;
   isOnline: boolean;
 }
+const LS_USER_SETTINGS_KEY = "userSettings";
+
+const DEFAULT_USER_SETTINGS: FormSettings = {
+  isDiscountVisible: true,
+  isEquallyVisible: true,
+  view: "user",
+  user: undefined,
+  total: 0,
+  discountPercent: 0,
+  discount: 0,
+  isPercentage: true,
+  isOnline: false,
+};
 
 export const PartySettingsProvider: FC<{
   children: React.ReactNode;
   isOnline: boolean;
 }> = ({ children, isOnline }) => {
   const currentUser = JSON.parse(localStorage.getItem("user") || "{}") || {};
+  const userSettings =
+    JSON.parse(localStorage.getItem(LS_USER_SETTINGS_KEY) || "{}") || {};
+
+  const { partyId } = useParams();
+
+  const defaultValues =
+    Object.values(userSettings).length > 0 && userSettings.partyId === partyId
+      ? { ...DEFAULT_USER_SETTINGS, ...userSettings, isOnline }
+      : {
+          ...DEFAULT_USER_SETTINGS,
+          user: currentUser,
+          isOnline,
+        };
 
   const handlers = useForm<FormSettings>({
-    defaultValues: {
-      isDiscountVisible: true,
-      isEquallyVisible: true,
-      view: "user",
-      user: currentUser,
-      total: 0,
-      discountPercent: 0,
-      discount: 0,
-      isPercentage: true,
-      isOnline,
-    },
+    defaultValues,
     resolver: yupResolver(partySettingsSchema),
     mode: "all",
   });
@@ -42,6 +59,23 @@ export const PartySettingsProvider: FC<{
   const discount = handlers.watch("discount");
   const isPercentage = handlers.watch("isPercentage");
   const total = handlers.watch("total");
+
+  useEffect(() => {
+    return () => {
+      const partySettings = handlers.getValues();
+      localStorage.setItem(
+        LS_USER_SETTINGS_KEY,
+        JSON.stringify({
+          isDiscountVisible: partySettings.isDiscountVisible,
+          isEquallyVisible: partySettings.isEquallyVisible,
+          view: partySettings.view,
+          user: partySettings.user,
+          partyId,
+        })
+      );
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (view === "party") {
