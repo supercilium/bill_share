649f1ba71852664ff2a3f37fd0899f70021d0d49
Mary <30400314+supercilium@users.noreply.github.com>
2023-12-24 19:53:25 +0300

    added online/offline event handlers (#37)

--- src/layouts/heroLayout.tsx
+++ src/layouts/heroLayout.tsx
@@ -1,14 +1,10 @@
 import { FC, ReactNode } from "react";
-import { Footer } from "../components";
-import { useTranslation } from "react-i18next";
 
 interface PlainLayoutInterface {
   children: ReactNode;
 }
 
 export const HeroLayout: FC<PlainLayoutInterface> = ({ children }) => {
-  const { t } = useTranslation();
-
   return (
     <section className="hero is-fullheight">
       <div className="hero-body">{children}</div>
